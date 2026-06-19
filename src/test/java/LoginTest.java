2cbfd1a09e72a34e3831de00ed35b13b3b3ebf62
Natalia Beskurnikova <bskurnikova@yandex.ru>
2026-06-19 12:23:18 +0300

    Homework_2

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,14 +1,30 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class LoginTest {
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
     @Test
     public void checkLogin() {
-        WebDriver browser = new ChromeDriver();
-        browser.get("https://www.saucedemo.com");
-        browser.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("standard_user");
-        browser.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        String titleName = driver.findElement(By.xpath("//*[@class='title']")).getText();
+
+        assertEquals(titleName, "Products", "Заголовок страницы  не соответствует");
+    }
+
+    @Test
+    public void checkIncorrectLogin() {
+        driver.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        boolean isTitleVisible = driver.findElement(By.xpath("//h3[@data-test='error']")).isDisplayed();
+        String errorText = driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
+
+        assertTrue(isTitleVisible);
+        assertEquals(errorText, "Epic sadface: Username is required");
     }
 }
