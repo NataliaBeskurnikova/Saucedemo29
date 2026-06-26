f1750342a049d9935a39c39f0037424ead202736
Natalia Beskurnikova <bskurnikova@yandex.ru>
2026-06-26 22:51:47 +0300

    Homework_3

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,15 +3,14 @@ package tests;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pages.LoginPage;
 import pages.ProductsPage;
 
+import java.time.Duration;
+
 public class BaseTest {
-    private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
     WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
@@ -20,8 +19,10 @@ public class BaseTest {
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
+        options.addArguments("guest");
         options.addArguments("headless");
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
 
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -24,4 +24,34 @@ public class LoginTest extends BaseTest {
         assertTrue(loginPage.isErrorDisplayed());
         assertEquals(loginPage.getErrorText(), "Epic sadface: Username is required");
     }
+
+    @Test
+    public void checkIncorrectPassword() {
+        loginPage.open();
+        loginPage.login("standard_user", "");
+
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Password is required");
+    }
+
+    @Test
+    public void checkLockedOutUser() {
+        loginPage.open();
+        loginPage.login("locked_out_user", "secret_sauce");
+
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Sorry, this user has been locked out.");
+    }
+
+    @Test
+    public void checkIncorrectLogin2() {
+        loginPage.open();
+        loginPage.login("Standard_user", "secret_sauce");
+
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Username and password do not match any user in this service");
+    }
 }
