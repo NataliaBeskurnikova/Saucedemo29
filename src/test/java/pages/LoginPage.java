569e02135819be8e7bbebc854fdfe1ddb38807c6
Natalia Beskurnikova <bskurnikova@yandex.ru>
2026-06-29 00:30:31 +0300

    Modified_29/06/26

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,19 +3,18 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
     private final By loginInput = By.xpath("//*[@placeholder='Username']");
     private final By passwordInput = By.xpath("//input[@placeholder='Password']");
     private final By submitButton = By.cssSelector("#login-button");
     private final By error = By.xpath("//h3[@data-test='error']");
-    WebDriver driver;
 
     public LoginPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public void open() {
-        driver.get("https://www.saucedemo.com");
+        driver.get(BASE_URL);
     }
 
     public void login(String login, String password) {
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -7,51 +8,22 @@ import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void checkLogin() {
-        loginPage.open();
-        loginPage.login("standard_user", "secret_sauce");
-
-        assertEquals(productsPage.getTitle(), "Products", "Заголовок страницы  не соответствует");
+    @DataProvider(name = "incorrectLoginData")
+    public Object[][] loginData() {
+        return new Object[][]{
+                {"", "secret_sauce", "Epic sadface: Username is required"},
+                {"standard_user", "", "Epic sadface: Password is required"},
+                {"locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."},
+                {"Standard_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"}
+        };
     }
 
-    @Test
-    public void checkIncorrectLogin() {
+    @Test(dataProvider = "incorrectLoginData")
+    public void checkIncorrectLogin(String user, String password, String errorMessage) {
         loginPage.open();
-        loginPage.login("", "secret_sauce");
-
+        loginPage.login(user, password);
 
         assertTrue(loginPage.isErrorDisplayed());
-        assertEquals(loginPage.getErrorText(), "Epic sadface: Username is required");
-    }
-
-    @Test
-    public void checkIncorrectPassword() {
-        loginPage.open();
-        loginPage.login("standard_user", "");
-
-
-        assertTrue(loginPage.isErrorDisplayed());
-        assertEquals(loginPage.getErrorText(), "Epic sadface: Password is required");
-    }
-
-    @Test
-    public void checkLockedOutUser() {
-        loginPage.open();
-        loginPage.login("locked_out_user", "secret_sauce");
-
-
-        assertTrue(loginPage.isErrorDisplayed());
-        assertEquals(loginPage.getErrorText(), "Epic sadface: Sorry, this user has been locked out.");
-    }
-
-    @Test
-    public void checkIncorrectLogin2() {
-        loginPage.open();
-        loginPage.login("Standard_user", "secret_sauce");
-
-
-        assertTrue(loginPage.isErrorDisplayed());
-        assertEquals(loginPage.getErrorText(), "Epic sadface: Username and password do not match any user in this service");
+        assertEquals(loginPage.getErrorText(), errorMessage);
     }
 }
