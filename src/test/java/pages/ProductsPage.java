edeccf4da9c5d1e81eeb18fc04e8ffbb6f1e7ef9
Natalia Beskurnikova <bskurnikova@yandex.ru>
2026-07-13 19:58:06 +0300

    ifat-5

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -40,4 +40,10 @@ public class ProductsPage extends BasePage {
     public String getCounterValue() {
         return driver.findElement(cartCounter).getText();
     }
+
+    public String checkCounterColor() {
+        return driver.findElement(counter).getCssValue("background-color");
+    }
+
+
 }
--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -2,21 +2,33 @@ package tests;
 
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static org.testng.Assert.assertEquals;
 
 public class ProductsTest extends BaseTest {
     @Test
     public void checkGoodsAdded() {
+
+        List<String> goddsList =
+                List.of("Sauce Labs Bolt T-Shirt",
+                        "Sauce Labs Bike Light",
+                        "Test.allTheThings() T-Shirt (Red)");
+
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
 
         assertEquals(productsPage.getTitle(), "Products", "Заголовок страницы не соответствует");
+        productsPage.addGoodsToCart(4);
 
-        productsPage.addGoodsToCart("Sauce Labs Bolt T-Shirt");
-        productsPage.addGoodsToCart(0);
+        for (String goods : goddsList) {
+            productsPage.addGoodsToCart(goods);
+        }
+
 
         assertEquals(productsPage.checkCounterValue(), "rgba(226, 35, 26, 1)");
         assertEquals(productsPage.isCartCounterDisplayed(), true);
         assertEquals(productsPage.getCounterValue(), "2");
+        assertEquals(productsPage.checkCounterValue(), "4");
     }
 }
